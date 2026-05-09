4d97b1d0b3a8fdd29c37394915d0b3c851f81fed
dalperen1212 <dalperen1212@gmail.com>
2026-05-09 20:39:28 +0300

    Remove merhaba class and its instance

--- main.py
+++ main.py
@@ -1,9 +1,1 @@
-class merhaba:
-    def __init__(self,name,yaş):
-        self.name = name
-        self.yaş = yaş
-    def __str__(self):
-        return f"{self.isim} kişisi {self.yaş} yaşında"
-        
-alperen = merhaba('alperen',15)
-print(alperen.yaş)
+
